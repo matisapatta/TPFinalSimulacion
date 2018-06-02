411c97c303d82834e6856c07b4004c45c9306fba
matisapatta <mati.sapatta@gmail.com>
2018-06-01 21:11:47 -0300

    Armado de servidor

--- python/init.py
+++ python/init.py
@@ -1,1 +1,18 @@
-develop
+import sys
+import json
+import cgi
+
+print("Content-Type: application/json")
+
+result = {}
+result['success'] = True
+result['message'] = "The command Completed Successfully"
+#result['keys'] = ",".join(fs.keys())
+
+d = {}
+for k in fs.keys():
+    d[k] = fs.getvalue(k)
+
+result['data'] = d
+print(json.dumps(result,indent=1))
+print("\n")
