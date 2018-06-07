4ff6aecc9ca47521a5059d6c5fb90b4a2d6e86e5
matisapatta <mati.sapatta@gmail.com>
2018-06-06 21:24:23 -0300

    agregado servidor en node

---
+++ server/server.js
@@ -0,0 +1,53 @@
+/*
+Dependencias
+ */
+const express = require('express')
+const cors = require('cors');
+const app = express()
+
+app.use(cors());
+app.get('/', function(req, res) {
+  res.send('Hello World!!')
+});
+
+app.get('/datos/',function(req,res){
+    res.json(
+        [{
+        x: ['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'],
+        y: [1, 3, 6],
+        type: 'scatter'
+        }]
+    );
+        
+})
+
+// // Pido estado de una sala
+// app.get('/sala/:name', function(req, res) {
+//   Sala.findOne({name: req.params.name}).then(function(response) {
+//     if (!response) return res.json({err: 'No la encontre'});
+//     res.json(response);
+//   });
+// });
+
+// Creo una sala
+// app.post('/sala/:name', function (req, res) {
+//   const sala = new Sala({ name: req.params.name });
+//   sala.save().then(function () {
+//     res.json({msg: 'sala creada ' +  sala.name});
+//   });
+// });
+
+// app.post('/sala/:name/ocupar', function (req, res) {
+//   Sala.findOne({name: req.params.name}).then(function(sala) {
+//     if (!sala) return res.json({err: 'No la encontre'});
+
+//     sala.disponible = false;
+//     sala.save().then(function() {
+//       res.send("Sala " +  sala.name + " ocupada.");
+//     });
+//   });
+// });
+
+app.listen(3000, function() {
+  console.log('Example app listening on port 3000!')
+});
