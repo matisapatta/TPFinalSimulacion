b452e705d9fe361c81fc060985aca7d66878a1a0
matisapatta <mati.sapatta@gmail.com>
2018-06-14 19:52:52 -0300

    added json example

--- server/server.js
+++ server/server.js
@@ -8,19 +8,19 @@ const app = express()
 
 
 app.use(cors());
-app.get('/', function(req, res) {
+app.get('/', function (req, res) {
   res.send('Hello World!!')
 });
 
-app.get('/datos/',function(req,res){
-    res.json(
-        [{
-        x: ['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'],
-        y: [1, 3, 6],
-        type: 'scatter'
-        }]
-    );
-        
+app.get('/datos/', function (req, res) {
+  res.json(
+    [{
+      x: ['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'],
+      y: [1, 3, 6],
+      type: 'scatter'
+    }]
+  );
+
 })
 
 // // Pido estado de una sala
@@ -51,13 +51,17 @@ app.get('/datos/',function(req,res){
 // });
 const path = './datos/lebaq-tasas-interes_ok.csv';
 
-csv()
-.fromFile(path).then((jsonObj) => {
-  console.log(jsonObj);
-});
+app.get('/datos1/', function (req, res) {
+  csv()
+    .fromFile(path).then((jsonObj) => {
+      console.log(jsonObj);
+      res.json(jsonObj);
+    });
+})
 
 
 
-app.listen(3000, function() {
+
+app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
