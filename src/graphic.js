bc1e24195097b6829dbcc002b3d4e505e50ab8d1
matisapatta <mati.sapatta@gmail.com>
2018-06-17 18:43:08 -0300

    cambiado

--- src/graphic.js
+++ src/graphic.js
@@ -1,8 +1,33 @@
-const g=new Dygraph('graph', "./src/ny-vs-sf.txt", {
-    legend: 'always',
-    title: 'NYC vs. SF',
-    showRoller: true,
-    rollPeriod: 14,
-    // customBars: true,
-    ylabel: 'Temperature (F)',
-});
+
+function calcular() {
+    const g = new Dygraph('graph', "./../server/datos/principales-tasas-interes-diarias_ok.csv", {
+        legend: 'always',
+        title: 'Título',
+        showRoller: true,
+        rollPeriod: 14,
+        // customBars: true,
+        ylabel: 'Tasa',
+        strokeWidth: 2,
+        visibility: [true, true, true, true]
+    });
+    setStatus();
+
+    function setStatus() {
+        document.getElementById("visibility").innerHTML =
+            g.visibility().toString();
+    }
+    
+    function change(el) {
+        g.setVisibility(parseInt(el.id), el.checked);
+        setStatus();
+    }
+    change(status);
+   
+
+    
+}
+
+
+
+
+
