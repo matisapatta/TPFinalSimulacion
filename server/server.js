5a449fcd75807c5990d41685c06e3110cbfb6707
Alan Arango <alanarangof@gmail.com>
2018-06-23 23:40:20 -0300

    Dolar con prediccion en json

--- server/server.js
+++ server/server.js
@@ -49,7 +49,11 @@ app.get('/datos/', function (req, res) {
 //     });
 //   });
 // });
+
+
 const path = './datos/lebaq-tasas-interes_ok.csv';
+const pathDolar = './datos/tipos-de-cambio-historicos_ok.csv';
+
 
 app.get('/datos1/', function (req, res) {
   csv()
@@ -59,9 +63,75 @@ app.get('/datos1/', function (req, res) {
     });
 })
 
-
-
-
 app.listen(3000, function () {
   console.log('Example app listening on port 3000!')
 });
+
+
+
+class jsonItem {
+    get json(){
+      return this.contenido;
+    }
+    set json(value){
+      this.contenido = value;
+    }
+  
+    getLastVal(){ //Ultimo valor de los datos actuales desde CSV
+      return this.contenido[this.contenido.length-1].valor;
+    }
+
+    getFutureDate(nDays){
+      var someDate = new Date(this.contenido[this.contenido.length-1].fecha);
+      someDate.setDate(someDate.getDate() + nDays); 
+    
+      var dd = someDate.getDate();
+      var mm = someDate.getMonth() + 1;
+      var y = someDate.getFullYear();
+    
+      var someFormattedDate = mm + '/'+ dd + '/'+ y;
+    
+      return someFormattedDate;
+    }
+
+    predictVal(t){//Predice el valor para dentro de 't' dias
+ 
+    }
+
+    jsonPrediction(days){//Devuelve el json con los datos mas los datos de predicciones de dias futuros para los proximos t dias
+ 
+    }
+  
+  }
+class dolarObj extends jsonItem {
+// formula dolar: Yt = 14.2998 - 0.000676×t + 0.000009×t^2 + 0.43415R, -1 < R < 1
+  
+  predictVal(t){//Predice el valor del dolar para dentro de 't' dias
+    var r = (Math.random() * 2) - 1;
+    return 14.2998 - 0.000676*(t+867) + 0.000009*Math.pow((t+867), 2) + 0.43415*r + (this.getLastVal() - 20.535);
+  }
+  jsonPrediction(days){//Devuelve el json con los datos mas los datos de predicciones de dias futuros para los proximos dias
+    var result = [];
+    var i;
+    for (i = 0; i <= days; i++) {
+      result[i] = {fecha: this.getFutureDate(i+1), valor: this.predictVal(i+1).toString()};
+    }
+    return result;
+  }
+
+}
+
+async function main() {
+  var dolar = new dolarObj();
+  dolar.json = await csv().fromFile(pathDolar);
+  var jsonPredicted = dolar.jsonPrediction(365);
+  console.log(jsonPredicted);
+}
+
+main();
+/*function GetPercentVariations(jsonRaw) {
+  var i;
+  for (i = 0; i < jsonRaw.length-1; i++) {
+    text += cars[i] + "<br>";
+  } 
+}*/
