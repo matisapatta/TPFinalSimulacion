655862a7be86a4bd110556bca8f98efb12e5e107
matisapatta <mati.sapatta@gmail.com>
2018-06-25 17:13:07 -0300

    cai finalizado

--- src/graphic.js
+++ src/graphic.js
@@ -1,8 +1,15 @@
 
+var g;
+var visibilidad = [$("#0").is(":checked"),$("#1").is(":checked"),$("#2").is(":checked"),$("#3").is(":checked")];
+var inversionTotal;
+function getInversionTotal(inversion){
+    inversionTotal = inversion;
+}
+
 function calcular(jsonData) {
     // const g = new Dygraph('graph', "./../server/datos/principales-tasas-interes-diarias_ok.csv", {
-        // console.log(jsonData);
-        const g = new Dygraph('graph', jsonData, {
+    // console.log(jsonData);
+    g = new Dygraph('graph', jsonData, {
         legend: 'always',
         title: '',
         showRoller: true,
@@ -10,24 +17,23 @@ function calcular(jsonData) {
         // customBars: true,
         ylabel: 'Dinero en $',
         strokeWidth: 2,
-        visibility: [true, true, true, true]
+        visibility: visibilidad
     });
+    // console.log(inversionTotal);
+    loadTable(inversionTotal);
+    changeStatus(status);
     setStatus();
+}
 
-    function setStatus() {
-        document.getElementById("visibility").innerHTML =
-            g.visibility().toString();
-    }
-    
-    function change(el) {
-        g.setVisibility(parseInt(el.id), el.checked);
-        setStatus();
-    }
-    
-    change(status);
-   
+function changeStatus(status) {
+    g.setVisibility(parseInt(status.id), status.checked);
+    loadTable(inversionTotal);
+    setStatus();
+}
 
-    
+function setStatus() {
+    document.getElementById("visibility").innerHTML =
+        g.visibility().toString();
 }
 
 
