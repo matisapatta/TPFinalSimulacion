a96209d10322e06ff9c4040a498dd5b5e6c1e627
matisapatta <mati.sapatta@gmail.com>
2018-07-07 20:25:04 -0300

    agregado grafico nuevo

--- src/graphic.js
+++ src/graphic.js
@@ -1,15 +1,59 @@
 
 var g;
+var g1;
+var g2;
+var g3;
+var g4;
 var visibilidad = [$("#0").is(":checked"),$("#1").is(":checked"),$("#2").is(":checked"),$("#3").is(":checked")];
 var inversionTotal;
 function getInversionTotal(inversion){
     inversionTotal = inversion;
 }
 
-function calcular(jsonData) {
+function calcular(jsonData,dolar) {
     // const g = new Dygraph('graph', "./../server/datos/principales-tasas-interes-diarias_ok.csv", {
     // console.log(jsonData);
     g = new Dygraph('graph', jsonData, {
+        legend: 'always',
+        title: '',
+        showRoller: true,
+        rollPeriod: 14,
+        // customBars: true,
+        ylabel: 'Dinero en $',
+        strokeWidth: 2,
+        visibility: visibilidad
+    });
+    g1 = new Dygraph(document.getElementById("dolargraf"), dolar, {
+        legend: 'always',
+        title: '',
+        showRoller: true,
+        rollPeriod: 14,
+        // customBars: true,
+        ylabel: 'Dinero en $',
+        strokeWidth: 2,
+        visibility: visibilidad
+    });
+    g2 = new Dygraph(document.getElementById("tasasgraf"), jsonData, {
+        legend: 'always',
+        title: '',
+        showRoller: true,
+        rollPeriod: 14,
+        // customBars: true,
+        ylabel: 'Dinero en $',
+        strokeWidth: 2,
+        visibility: visibilidad
+    });
+    g3 = new Dygraph(document.getElementById("lebacgraf"), jsonData, {
+        legend: 'always',
+        title: '',
+        showRoller: true,
+        rollPeriod: 14,
+        // customBars: true,
+        ylabel: 'Dinero en $',
+        strokeWidth: 2,
+        visibility: visibilidad
+    });
+    g4 = new Dygraph(document.getElementById("fcigraf"), jsonData, {
         legend: 'always',
         title: '',
         showRoller: true,
