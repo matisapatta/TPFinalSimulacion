9fce3b7880230228047386c9a2e5f4be5e9496d7
matisapatta <mati.sapatta@gmail.com>
2018-07-09 22:19:18 -0300

    -

--- server/server.js
+++ server/server.js
@@ -9,20 +9,13 @@ const csv = require('csvtojson');
 const app = express()
 const variable = 1;
 
+var port = process.env.PORT || 3000;
+
 app.use(cors());
 app.get('/', function (req, res) {
   res.send('Hello World!!')
 });
 
-app.get('/datos/', function (req, res) {
-  res.json(
-    [{
-      x: ['2013-10-04 22:23:00', '2013-11-04 22:23:00', '2013-12-04 22:23:00'],
-      y: [1, 3, 6],
-      type: 'scatter'
-    }]
-  );
-})
 
 
 // // Pido estado de una sala
@@ -60,8 +53,8 @@ app.get('/datos/', function (req, res) {
 //     });
 // })
 
-app.listen(3000, function () {
-  console.log('Example app listening on port 3000!')
+app.listen(port, function () {
+  console.log('Example app listening on port' + port);
 });
 
 const pathTasaInt = './datos/principales-tasas-interes-diarias_ok.csv';
